e154c52105b57177e1a19898737b06228219cef1
mikan-juice <61767467+mikan-juice@users.noreply.github.com>
2020-10-16 22:09:31 +0900

    update purchases markup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :purchases, only: [:index, :new]
+  get 'purchases/edit', to: 'purchases#edit'
 end
