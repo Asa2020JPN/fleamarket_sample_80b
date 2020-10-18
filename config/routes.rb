fbc801681d12d3761b0a067dc012b0cb5481dd68
hajime miyagi <hajime.prog@gmail.com>
2020-10-18 16:58:57 +0900

    identificationsのルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+    devise_scope :user do
+    get 'identifications', to: 'users/registrations#new__identification'
+    post 'identifications', to: 'users/registrations#create__identification'
+  end
   root 'items#index'
   resources :purchases, only: [:index, :new]
   get 'purchases/edit', to: 'purchases#edit'
