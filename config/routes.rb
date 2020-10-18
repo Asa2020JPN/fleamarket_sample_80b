045eb3b6f16e5cf7c3c465897e9f2c8beb3b4b38
mikan-juice <61767467+mikan-juice@users.noreply.github.com>
2020-10-18 14:59:03 +0900

    deviseのコントローラーをuserに継承

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
   root 'items#index'
   resources :purchases, only: [:index, :new]
   get 'purchases/edit', to: 'purchases#edit'
