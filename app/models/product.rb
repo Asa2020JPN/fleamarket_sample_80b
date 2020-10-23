b569d84acc6d73701c7cb338c6596d71ae3ada5f
jirou6699 <shundom09@gmail.com>
2020-10-23 14:35:37 +0900

    set-validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
-  # belongs_to :saler, class_name: "User"
-  # belongs_to :buyer, class_name: "User"
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
 
@@ -9,4 +7,10 @@ class Product < ApplicationRecord
   belongs_to_active_hash :shipping_cost
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping
+  
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :status_id, :prefecture_id, :shipping_cost_id, :shipping_id, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
