d86d0e13fdcca7ad0ab1ba994c688d2141a8bb6f
hajime miyagi <hajime.prog@gmail.com>
2020-10-24 20:44:51 +0900

    説明の追加。不要文削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,19 +9,19 @@ describe User do
       expect(user).to be_valid
     end
 
-    it "nicknameが空の場合" do
+    it "nicknameが空　登録できない" do
       user = build(:user, nickname: nil)
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank") 
     end
 
-    it "passwordが空の場合" do
+    it "passwordが空　登録できない" do
       user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("can't be blank") 
     end
 
-    it "passwordが6文字以下の場合" do
+    it "passwordが6　登録できない" do
       user = build(:user, password: "0000")
       user.valid?
       expect(user.errors[:password]).to include("is too short (minimum is 7 characters)") 
@@ -33,88 +33,35 @@ describe User do
       expect(user).to be_valid
     end
 
-    it "passwordが有りで、password_confirmationが空の場合" do
+    it "passwordが有りで、password_confirmationが空の場合　登録できない" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("doesn't match Password") 
     end
 
-    it "emailが空の場合" do
+    it "emailが空の場合　登録できない" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank") 
     end
 
-    it "emailに＠がない場合" do
+    it "emailに＠がない場合　登録できない" do
       user = build(:user, email: 'aaa')
       user.valid?
       expect(user.errors[:email]).to include("is invalid", "is invalid") 
     end
 
-    it "emailの＠の前にアドレスがない場合" do
+    it "emailの＠の前にアドレスがない場合　登録できない" do
       user = build(:user, email: '@aaa')
       user.valid?
       expect(user.errors[:email]).to include("is invalid", "is invalid") 
     end
 
-    it "重複したemail" do
+    it "重複したemail　登録できない" do
       user1 = create(:user)
       user2 = build(:user, email: user1.email)
       user2.valid?
       expect(user2.errors[:email]).to include("has already been taken")
     end
-    # it "passwordが空の場合" do
-    #   user = build(:user, password: nil)
-    #   user.valid?
-    #   expect(user.errors[:password]).to include("can't be blank") 
-    # end
-    # it "passwordが空の場合" do
-    #   user = build(:user, password: nil)
-    #   user.valid?
-    #   expect(user.errors[:password]).to include("can't be blank") 
-    # end
-    # it "passwordが空の場合" do
-    #   user = build(:user, password: nil)
-    #   user.valid?
-    #   expect(user.errors[:password]).to include("can't be blank") 
-    # end
-    # it "passwordが空の場合" do
-    #   user = build(:user, password: nil)
-    #   user.valid?
-    #   expect(user.errors[:password]).to include("can't be blank") 
-    # end
-    # it "passwordが空の場合" do
-    #   user = build(:user, password: nil)
-    #   user.valid?
-    #   expect(user.errors[:password]).to include("can't be blank") 
-    # end
-
   end
-
 end
-
-
-
-    # it "nicnameが空の場合" do
-    #   user = build(:user, nickname: nil)
-    #   user.valid?
-    #   expect(user.errors[:nickname]).to include("can't be blank") 
-    # end
-
-    # it "nicnameが空の場合" do
-    #   user = build(:user, nickname: nil)
-    #   user.valid?
-    #   expect(user.errors[:nickname]).to include("can't be blank") 
-    # end
-
-    # it "nicnameが空の場合" do
-    #   user = build(:user, nickname: nil)
-    #   user.valid?
-    #   expect(user.errors[:nickname]).to include("can't be blank") 
-    # end
-
-    # it "nicnameが空の場合" do
-    #   user = build(:user, nickname: nil)
-    #   user.valid?
-    #   expect(user.errors[:nickname]).to include("can't be blank") 
-    # end
