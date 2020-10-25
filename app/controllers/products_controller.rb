d8b14c6e242a5762f0cc4070a836dbe80e219107
jirou6699 <shundom09@gmail.com>
2020-10-25 21:43:52 +0900

    Add-category_id-products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -50,7 +50,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :detail, :price, :status_id, :prefecture_id, :shippingcost_id, :shipping_id, images_attributes: [:image, :_destroy, :id])
+    params.require(:product).permit(:name, :detail, :price, :status_id, :prefecture_id, :category_id, :shippingcost_id, :shipping_id, images_attributes: [:image, :_destroy, :id])
   end
 
   def set_product
