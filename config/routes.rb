da2f243fc05f6dc98840e20a746a190c8a77d689
Asa2020JPN <tasahina0703@icloud.com>
2020-11-05 23:20:49 +0900

    Create-mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'items#index'
+  # root'users#logout'
+  # root 'posts#index'
+  get 'mypages', to:'mypages#index'
+  get 'users', to: 'users#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+#
