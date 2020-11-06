69dcfaaf5b06dc0729540cef8e59e30d6219532b
koemaru <yuhaya.1027@gmail.com>
2020-11-06 22:44:28 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root 'tops#index'
+  root 'products#index'
   get 'purchases/edit', to: 'purchases#edit'
   resources :purchases, only: [:index, :new]
-  resources :tops, only: [:index, :show]
-  resources :products, only: [:index]
-  resources :products, only: [:new, :create, :edit, :create] do
+  resources :categories, only: [:index, :show]
+  resources :products, only: [:index, :show, :new, :create, :edit, :create] do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
