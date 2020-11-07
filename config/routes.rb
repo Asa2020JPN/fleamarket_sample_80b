893fbf2d5e7f8441de1cbf06fa348bfc3bd8f4dd
koemaru <yuhaya.1027@gmail.com>
2020-11-07 11:10:48 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:show]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
