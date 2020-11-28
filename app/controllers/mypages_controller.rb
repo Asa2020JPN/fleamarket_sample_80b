1817bb5ad91b914ac474a5ddbbc31cec06cb801c
Asa2020JPN <tasahina0703@icloud.com>
2020-11-28 15:43:06 +0900

    update-file10

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,6 +3,7 @@ class MypagesController < ApplicationController
     @saler = Product.where(saler_id: current_user.id)
     @buyer = Product.where(buyer_id: current_user.id)
   end
+
   def show
     if user_signed_in?
       @selected_product = Product.includes(:images).find(params[:id])
@@ -10,5 +11,11 @@ class MypagesController < ApplicationController
     else
       redirect_to new_user_session_path
     end
+  end
+  
+  def destroy
+    delete_product = Product.find(params[:id])
+    delete_product.destroy
+    redirect_to mypages_path
   end
 end
