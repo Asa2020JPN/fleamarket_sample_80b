d3f7a85e70cebeee57bc69579c829baa527ed393
jirou6699 <shundom09@gmail.com>
2020-12-03 19:56:19 +0900

    Update-html/haml

--- app/models/product.rb
+++ app/models/product.rb
@@ -21,6 +21,5 @@ class Product < ApplicationRecord
   validates :shippingcost_id, presence: true
   validates :shipping_id, presence: true
   validates :price, presence: true
-  # numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
 end
